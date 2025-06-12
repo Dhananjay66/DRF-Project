9e959efe59490331545fa2d43e6c752f8d6a590d
Dhananajay <pratapsinghd665@gmail.com>
2025-06-12 17:24:51 +0530

    Some Final Changes

--- Django_Rest_Main/urls.py
+++ Django_Rest_Main/urls.py
@@ -1,12 +1,42 @@
+# from django.contrib import admin
+# from django.urls import path, include
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     # Web Application Endpoint
+#     path('students/', include('students.urls')),
+
+#     # API Endpoint
+#     path('api/v1/', include('api.urls')),
+    
+# ]
+
+
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+from rest_framework.documentation import include_docs_urls
+
+def home(request):
+    html = """
+    <h1>🚀 Welcome to the Django REST API</h1>
+    <ul>
+        <li><a href="/admin/">Admin Panel</a></li>
+        <li><a href="/api/">API</a></li>
+        <li><a href="/students/">Students API</a></li>
+        <li><a href="/employees/">Employees API</a></li>
+        <li><a href="/blogs/">Blogs API</a></li>
+        <li><a href="/docs/">API Docs</a></li>
+    </ul>
+    """
+    return HttpResponse(html)
 
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
-    # Web Application Endpoint
+    path('api/', include('api.urls')),
     path('students/', include('students.urls')),
-
-    # API Endpoint
-    path('api/v1/', include('api.urls')),
-    
+    path('employees/', include('employees.urls')),
+    path('blogs/', include('blogs.urls')),
+    path('docs/', include_docs_urls(title='API Documentation')),
 ]
