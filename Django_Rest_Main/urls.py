7e6a6e7ba75c8f74e3788f2b85a83bb481557546
Dhananajay <pratapsinghd665@gmail.com>
2025-06-13 10:21:06 +0530

    Some modification in Templates

--- Django_Rest_Main/urls.py
+++ Django_Rest_Main/urls.py
@@ -1,34 +1,9 @@
-# from django.contrib import admin
-# from django.urls import path, include
-
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     # Web Application Endpoint
-#     path('students/', include('students.urls')),
-
-#     # API Endpoint
-#     path('api/v1/', include('api.urls')),
-    
-# ]
-
-
 from django.contrib import admin
 from django.urls import path, include
-from django.http import HttpResponse
-from rest_framework.documentation import include_docs_urls
+from django.shortcuts import render
 
 def home(request):
-    html = """
-    <h1>🚀 Welcome to the Django REST API</h1>
-    <ul>
-        <li><a href="/admin/">Admin Panel</a></li>
-        <li><a href="/api/">API</a></li>
-        <li><a href="/api/students/">Students API</a></li>
-        <li><a href="/api/employees/">Employees API</a></li>
-        <li><a href="/api/blogs/">Blogs API</a></li>
-    </ul>
-    """
-    return HttpResponse(html)
+    return render(request, 'home.html')
 
 urlpatterns = [
     path('', home),
